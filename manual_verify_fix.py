a2c9c70c56d0cc9e3244d7574da92297470948cb
Yashwant <10a19yashwant@gmail.com>
2025-12-01 10:35:24 +0530

    'Removed Cheker Files'

--- manual_verify_fix.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-with open('src/agents.py', 'r') as f:
-    content = f.read()
-    if 'async def run_async(self, input_str: str, **kwargs):' in content:
-        print("SUCCESS: run_async is defined in src/agents.py")
-    else:
-        print("FAILURE: run_async is NOT defined in src/agents.py")
